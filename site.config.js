7c649988c1f168161cd7f1452daa9bed3e1e60aa
xyfx666 <51446918+xyfx666@users.noreply.github.com>
2021-05-26 20:25:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'xyfx666/notionblog',
+  utterancesGitHubRepo: 'xyfx666/xyblogtalk',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
